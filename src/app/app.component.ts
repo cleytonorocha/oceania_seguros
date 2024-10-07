2e6ef52b37f2ad818b756697b4c4adfc90975b4f
cleytonorocha <cleytoodeoliveirarocha@gmail.com>
2024-10-07 15:06:11 -0300

    feat: plans component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { ApresentacaoComponent } from './apresentacao/apresentacao.component';
 import { CardsComponent } from './cards/cards.component';
+import { PlansComponent } from './plans/plans.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, ApresentacaoComponent, CardsComponent],
+  imports: [RouterOutlet, NavbarComponent, ApresentacaoComponent, PlansComponent, CardsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/plans/plans.component.ts
@@ -0,0 +1,133 @@
+import { NgClass } from '@angular/common';
+import { Component } from '@angular/core';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+
+@Component({
+  selector: 'app-plans',
+  standalone: true,
+  imports: [
+    NgClass,
+    CarouselModule]
+  ,
+  templateUrl: './plans.component.html',
+  styleUrl: './plans.component.scss'
+})
+export class PlansComponent {
+  hoveredIndex: number | null = null;
+
+  planOptions: PlanOption[] = [
+    {
+      icon: 'fas fa-user-shield text-success fa-2x me-3',
+      title: 'Plano Saúde Individual',
+      description: 'Ideal para quem quer cobertura individual com ampla rede de hospitais e clínicas.'
+    },
+    {
+      icon: 'fas fa-users text-success fa-2x me-3',
+      title: 'Plano Saúde Familiar',
+      description: 'Cobre toda a sua família, oferecendo segurança e tranquilidade para quem você ama.'
+    },
+    {
+      icon: 'fas fa-briefcase-medical text-success fa-2x me-3',
+      title: 'Plano Empresarial',
+      description: 'Opção flexível para empresas de todos os tamanhos. Benefícios para seus funcionários.'
+    },
+    {
+      icon: 'fas fa-heartbeat text-success fa-2x me-3',
+      title: 'Plano Completo',
+      description: 'Cobertura total com diversos benefícios em saúde, odontologia, e bem-estar.'
+    }
+  ];
+
+  benefictOptions: PlanOption[] = [
+    {
+      icon: 'fas fa-stethoscope text-success fa-2x me-3',
+      title: 'Atendimento Médico de Qualidade',
+      description: 'Consultas com especialistas em diversas áreas para garantir seu cuidado.'
+    },
+    {
+      icon: 'fas fa-tooth text-success fa-2x me-3',
+      title: 'Plano Odontológico',
+      description: 'Cobertura completa para tratamentos dentários preventivos e de emergência.'
+    },
+    {
+      icon: 'fas fa-heart text-success fa-2x me-3',
+      title: 'Suporte Emergencial 24h',
+      description: 'Serviço de emergência disponível 24 horas por dia para situações críticas.'
+    },
+    {
+      icon: 'fas fa-ambulance text-success fa-2x me-3',
+      title: 'Ambulâncias Rápidas',
+      description: 'Cobertura para transporte de emergência rápido e eficiente para hospitais.'
+    }
+  ];
+
+  imageList: ImageItem[] = [
+    {
+      title: "Usi Saúde",
+      imagePath: "assets/logos/usisaude.jpg",
+      alt: "Logo da Usisaúde",
+      link: ''
+    },
+    {
+      title: "You Saúde",
+      imagePath: "assets/logos/yousaude.jpg",
+      alt: "Logo da You Saúde",
+      link: ''
+    },
+    {
+      title: "Notre Dame",
+      imagePath: "assets/logos/notredame.jpg",
+      alt: "Logo da NotreDame",
+      link: ''
+    },
+    {
+      title: "Hapvida",
+      imagePath: "assets/logos/hapvida.jpg",
+      alt: "Logo da Hapvida",
+      link: ''
+    },
+    {
+      title: "Med Senior",
+      imagePath: "assets/logos/madsenior.jpg",
+      alt: "Logo da MedSenior",
+      link: ''
+    },
+    {
+      title: "Golden Cross",
+      imagePath: "assets/logos/goldencross.jpg",
+      alt: "Logo da Golden Cross",
+      link: ''
+    },
+    {
+      title: "Amil",
+      imagePath: "assets/logos/amil.jpg",
+      alt: "Logo da Amil",
+      link: ''
+    },
+    {
+      title: "Sul América",
+      imagePath: "assets/logos/sulamerica.jpg",
+      alt: "Logo da SulAmérica",
+      link: ''
+    },
+    {
+      title: "Unimed",
+      imagePath: "assets/logos/unimed.jpg",
+      alt: "Logo da Unimed Belo Horizonte",
+      link: ''
+    }
+  ];
+
+}
+interface ImageItem {
+  title: string;
+  link: string;
+  imagePath: string;
+  alt: string;
+}
+
+interface PlanOption {
+  icon: string;
+  title: string;
+  description: string;
+}
